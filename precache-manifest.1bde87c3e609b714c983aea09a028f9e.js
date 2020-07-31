3bc870a9bb031931e7d9d0f945a3c696623a6e8e
askelej <askeltechno@hotmail.com>
2020-07-31 05:01:16 +0200

    Updates

---
+++ precache-manifest.1bde87c3e609b714c983aea09a028f9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2412bf079fb5127df9884702bd98ddc",
+    "url": "/orbit-calculator/index.html"
+  },
+  {
+    "revision": "7249b4b1cdcedde2357f",
+    "url": "/orbit-calculator/static/css/main.986588c8.chunk.css"
+  },
+  {
+    "revision": "a21bcd1c36dce4a3f2e0",
+    "url": "/orbit-calculator/static/js/2.2b3d49cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/orbit-calculator/static/js/2.2b3d49cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7249b4b1cdcedde2357f",
+    "url": "/orbit-calculator/static/js/main.df1fa991.chunk.js"
+  },
+  {
+    "revision": "0490ce01e829b48160e0",
+    "url": "/orbit-calculator/static/js/runtime-main.ee8ab64c.js"
+  },
+  {
+    "revision": "c1e9a816886d6681b4e02f3fc94a93ee",
+    "url": "/orbit-calculator/static/media/planet.c1e9a816.jpg"
+  }
+]);
